e66658e3d22b10dfa4b7f1a50072b3329c1c6faa
Ben Davis <contact.warpzone@gmail.com>
2018-05-31 10:33:15 -0600

    Finished working binary search with static array

--- Ch11ExampleProjects/BinarySearchProject/src/binarysearchproject/BinarySearchProject.java
+++ Ch11ExampleProjects/BinarySearchProject/src/binarysearchproject/BinarySearchProject.java
@@ -14,16 +14,68 @@ package binarysearchproject;
 import java.util.*;             // Scanner
 public class BinarySearchProject {
     // Binary Search helper method
+    public static int binarySearch(int[] array, int first,
+            int last, int target) {
+        int mid;                // holder for middle point
+        int index;              // returned array position
+        
+        if (first == last) {    // stopping condition
+            if (array[first] == target) {
+                index = first;
+            }
+            else {
+                index = -1;
+            }
+        }
+        else {
+            mid = (last + first) / 2;
+            if (target > array[mid]) {
+                first = mid + 1;
+            }
+            else {
+                last = mid;
+            }
+            index = binarySearch(array, first, last, target);
+        }
+        return index;
+    }
+    
+    //***************************************************************
     
     // Print helper method
+    public static void printArray(int[] array) {
+        Scanner stdIn = new Scanner(System.in);
+        int target = 0;
+        System.out.print("Array contains: ");
+        for (int i : array) {
+            System.out.printf("%3d", i);
+        }
+        do {
+            System.out.print("\nWhich number should I search for? "
+                + "(-1 to stop) ");
+            target = stdIn.nextInt();
+            if (binarySearch(array, 0, (array.length + 1), target)
+                    >= 0) {
+                System.out.println(target + " was found in position "
+                    + (binarySearch(array, 0, (array.length + 1), target) + 1));
+            }
+            else if (target == -1) {
+                System.out.println("Good bye.");
+            }
+            else {
+                System.out.println("That number was not found.");
+            }
+        } while (target != -1);
+    }
+    
+    //***************************************************************
     
     public static void main(String[] args) {
-        Scanner stdIn = new Scanner(System.in);
         System.out.println("Binary Search by Ben Davis\n");
         int[] array = new int[] {5, 10, 15, 20, 25, 30, 35, 40, 45,
             50, 55, 60, 65, 70, 75, 80, 85, 90};
         
-        
+        printArray(array);
     }   // end main
     
 }   // end BinarySearchProject class
