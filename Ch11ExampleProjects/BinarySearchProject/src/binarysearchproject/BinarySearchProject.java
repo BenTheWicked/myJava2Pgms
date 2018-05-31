d0da22d8d992a44fa80e4b35d16dba494f8ebc0e
Ben Davis <contact.warpzone@gmail.com>
2018-05-31 10:08:49 -0600

    Build array and project template

--- Ch11ExampleProjects/BinarySearchProject/src/binarysearchproject/BinarySearchProject.java
+++ Ch11ExampleProjects/BinarySearchProject/src/binarysearchproject/BinarySearchProject.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/********************************************************************
+ * BinarySearchProject.java
+ * Ben Davis
+ * 
+ * After creating a sorted array, this program uses a recursive
+ * binary search (helper method) to find a user-specified element
+ * and report its position. A printing method (helper method) uses 
+ * a loop to display the array, ask the user for a value, search for
+ * the value, and return its position. The program must be able to
+ * find a value near the beginning or end of the array or to return
+ * a if the number was not found.
+ ********************************************************************/
 package binarysearchproject;
-
-/**
- *
- * @author BenTheWicked
- */
+import java.util.*;             // Scanner
 public class BinarySearchProject {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    // Binary Search helper method
     
-}
+    // Print helper method
+    
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.println("Binary Search by Ben Davis\n");
+        int[] array = new int[] {5, 10, 15, 20, 25, 30, 35, 40, 45,
+            50, 55, 60, 65, 70, 75, 80, 85, 90};
+        
+        
+    }   // end main
+    
+}   // end BinarySearchProject class
