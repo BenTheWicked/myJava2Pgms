d06020714c8b7dc7cf90f11ac8e926227b428b50
Ben Davis <contact.warpzone@gmail.com>
2018-06-01 14:01:56 -0600

    Copy AverageScore.java from text

---
+++ AverageScore/src/averagescore/AverageScore.java
@@ -0,0 +1,28 @@
+/*******************************************************************
+ * AverageScore.java
+ * Dean & Dean
+ * 
+ * This program averages input scores. (pg 530)
+ *******************************************************************/
+package averagescore;
+import java.util.Scanner;
+public class AverageScore {
+
+    public static void main(String[] args) {
+        double score;
+        double count = 0;
+        double totalScore = 0;
+        Scanner stdIn = new Scanner(System.in);
+        
+        System.out.print("Enter a score (or -1 to quit): ");
+        while ((score = stdIn.nextDouble()) != -1) {
+            count++;
+            totalScore += score;
+            System.out.print("Enter a score (or -1 to quit): ");
+        }
+        if (count > 0) {
+            System.out.println("Average score = " + totalScore / count);
+        }
+    }   // end main
+    
+}   // end AverageScore class
