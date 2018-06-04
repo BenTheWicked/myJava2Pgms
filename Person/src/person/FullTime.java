bdab752636780fe35f61524b7b4ba9540e8befec
Ben Davis <contact.warpzone@gmail.com>
2018-06-04 10:27:07 -0600

    Build FullTime.java from text to understand method overriding

---
+++ Person/src/person/FullTime.java
@@ -0,0 +1,26 @@
+/*********************************************************************
+ * FullTime.java
+ * Dean & Dean
+ * 
+ * This class describes a full-time employee. (pg 581)
+ *********************************************************************/
+package person;
+
+public class FullTime extends Employee {
+    private double salary = 0.0;
+    
+    //*****************************************************************
+    
+    public FullTime(String name, int id, double salary) {
+        super(name, id);    // calls the two-parameter Employee constructor
+        this.salary = salary;
+    }
+    
+    //*****************************************************************
+    
+    @Override
+    public void display() {
+        super.display();
+        System.out.printf(" salary: $%,.0f\n", salary);
+    }
+}   // end FullTime class
