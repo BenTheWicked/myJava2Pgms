7216d5fde5810b7b03cbbc060c865f4c223c8e22
Ben Davis <contact.warpzone@gmail.com>
2018-06-07 14:06:15 -0600

    Build Division2.java and add exception handling

--- Ch15ExampleProjects/Division2/src/division2/Division2.java
+++ Ch15ExampleProjects/Division2/src/division2/Division2.java
@@ -10,13 +10,44 @@ public class Division2 {
 
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
-        double dividend;
-        int divisor;
-        System.out.print("Enter dividend: ");
-        dividend = stdIn.nextDouble();
-        System.out.print("Enter divisor: ");
-        divisor = stdIn.nextInt();
-        System.out.println(dividend / divisor);
+        double dividend = 0.0;
+        int divisor = 0;
+        boolean valid = false;      // first attempt to catch errors.
+        do {
+            System.out.print("Enter dividend: ");
+            String numStr = stdIn.nextLine();
+            try {
+                dividend = Double.parseDouble(numStr);
+                valid = true;
+            }
+            catch (NumberFormatException nfe) {
+                System.out.println("Invalid entry.");
+            }
+        } while (!valid);
+        do {
+            valid = false;
+            System.out.print("Enter divisor: ");
+            String divStr = stdIn.nextLine();
+            try {
+                divisor = Integer.parseInt(divStr);
+                valid = true;
+            }
+            catch (NumberFormatException nfe) {
+                System.out.println("Invalid entry.");
+            }
+            if (valid && divisor == 0) {
+                System.out.println("No dividing by zero!");
+                valid = false;
+            }
+        } while (!valid);
+        try {
+            System.out.println(dividend / divisor);
+        }
+        catch (ArithmeticException ae) {
+            System.out.println("Sorry, something went wrong.");
+            System.out.println(ae.getClass());
+            System.out.println(ae.getMessage());
+        }
     }   // end main
     
 }   // end Division2 class
